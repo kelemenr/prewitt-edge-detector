c7ec75408507b845a1388cd8e3afa33bc4d94e2a
reka.kelemen@hotmail.hu <reka.kelemen@hotmail.hu>
2021-09-17 00:41:10 +0200

    Clean-up

--- edge_detection.py
+++ edge_detection.py
@@ -3,45 +3,47 @@ import matplotlib.pyplot as plt
 import skimage.io as io
 from skimage.util import img_as_float
 from scipy import ndimage
+import sys
+import cv2
 import os
 
 
 def prewitt_filter(img):
-    # Sobel kernels Gx, Gy
-    Gx = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
-    Gy = np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1]])
+    # Sobel kernels g_x, g_y
+    g_x = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
+    g_y = np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1]])
 
-    # Convolve matrix with kernel Gx, Gy
-    Gx = ndimage.filters.convolve(img, Gx)
-    Gy = ndimage.filters.convolve(img, Gy)
+    # Convolve matrix with kernel g_x, g_y
+    g_x = ndimage.filters.convolve(img, g_x)
+    g_y = ndimage.filters.convolve(img, g_y)
 
-    # Calculate magnitude G
-    G = np.sqrt(Gx ** 2 + Gy ** 2)
-    G = G / G.max() * 255
+    # Calculate magnitude g
+    g = np.sqrt(g_x ** 2 + g_y ** 2)
+    g = g / g.max() * 255
 
     # Calculate slope of the gradient (gradient's direction)
-    theta = np.arctan2(Gy, Gx)
+    theta = np.arctan2(g_y, g_x)
 
-    return G, theta
+    return g, theta
 
 
 def non_maximum_suppression(img, gradient):
     suppressed_image = np.zeros(img.shape)
-    A = gradient * 180.0 / 3.14159  # convert to degrees
+    a = gradient * 180.0 / 3.14159  # convert to degrees
     # Replace negative values
-    A[A < 0] += 180
+    a[a < 0] += 180
 
     # Identify edge direction using the angle matrix
-    for i in range(len(A[:, 0]) - 1):
-        for j in range(len(A[0, :]) - 1):
+    for i in range(len(a[:, 0]) - 1):
+        for j in range(len(a[0, :]) - 1):
             u, v = 255, 255
-            if (0 <= A[i, j] < 22.5) or (157.5 <= A[i, j] <= 180):  # 0°
+            if (0 <= a[i, j] < 22.5) or (157.5 <= a[i, j] <= 180):  # 0°
                 u, v = img[i, j + 1], img[i, j - 1]
-            elif 67.5 <= A[i, j] < 112.5:  # 90°
+            elif 67.5 <= a[i, j] < 112.5:  # 90°
                 u, v = img[i + 1, j], img[i - 1, j]
-            elif 112.5 <= A[i, j] < 157.5:  # 135°
+            elif 112.5 <= a[i, j] < 157.5:  # 135°
                 u, v = img[i - 1, j - 1], img[i + 1, j + 1]
-            elif 22.5 <= A[i, j] < 67.5:  # 45°
+            elif 22.5 <= a[i, j] < 67.5:  # 45°
                 u, v = img[i + 1, j - 1], img[i - 1, j + 1]
 
             # Check if the pixel in the direction has a higher \
@@ -64,14 +66,20 @@ def get_edges(img):
     return gradient, theta, suppressed_image
 
 
-entries = os.listdir('test_images/')
-entries = [str(i) for i in entries]
+def main():
+    try:
+        path = sys.argv[1]
+        file_name = os.path.basename(path)
+        image = io.imread(str(path), as_gray=True)
+    except:
+        raise SystemExit(f"Usage: {sys.argv[0]} <image_path>")
 
-for image in entries:
-    image = io.imread('test_images/' + image, as_gray=True)
     image = img_as_float(image)
 
     grad, t, suppressed = get_edges(image)
+
+    cv2.imwrite("output/prewitt_" + file_name, grad)
+    cv2.imwrite("output/non_max_" + file_name, suppressed)
 
     fig, (ax1, ax2, ax3) = plt.subplots(1, 3)
     fig.set_figheight(10)
@@ -86,3 +94,7 @@ for image in entries:
     ax3.imshow(suppressed, cmap='gray')
     ax3.set_title('Non-Maximum Suppression', fontsize=16)
     plt.show()
+
+
+if __name__ == "__main__":
+    main()
